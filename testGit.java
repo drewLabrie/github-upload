3b79ceba2a8f3df5da578395d8e19c960aa6c010
Drew Labrie <LabrieD2@wit.edu>
2021-02-03 16:26:24 -0500

    commit

---
+++ testGit.java
@@ -0,0 +1,8 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="0">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="848" startPos="26" endPos="26" selMode="0" offset="0" wrapCount="1" lang="Java" encoding="-1" userReadOnly="no" filename="C:\Program Files\Notepad++\change.log" backupFilePath="C:\Users\drewl\AppData\Roaming\Notepad++\backup\change.log@2021-02-03_160740" originalFileLastModifTimestamp="1589473024" originalFileLastModifTimestampHigh="30859206" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="7274574" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
